5cb66ae65958b5f81ca82948ef1b9ae14b8269ff
Valentin Lup <me@valentinlup.ro>
2021-07-20 16:08:06 +0300

    Added ENVIRONMENT variable in variables.cfg to make scripts universal (mainnet, testnet, devnet) Added check to see if ENVIRONMENT is correctly set inside variables.cfg Added proper description for the ENVIRONMENT variable Added an OVERRIDE_CONFIGVER variable for custom tags testing Added feedback if $OVERRIDE_CONFIGVER is set Added a new function to be able to add additional nodes Added menu entry for the add_node function Updated the menu to reflect the new function Removed arwen binary build - deprecated Changed node naming scheme to reflect the net on which nodes are running (mainnet, testnet, devnet) Refactor proxy start and stop functions Added selective start for nodes Added selective stop for nodes Added feedback on running elrond services in start and stop flows

---
+++ script.sh
@@ -0,0 +1,202 @@
+#!/bin/bash
+set -e
+
+#Make script aware of its location
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+source $SCRIPTPATH/config/variables.cfg
+source $SCRIPTPATH/config/functions.cfg
+source $SCRIPTPATH/config/menu_functions.cfg
+
+#Check if there are newer versions of the scripts available
+cd $SCRIPTPATH
+CURRENT_SCRIPTS_COMMIT=$(git show | grep  -m 1 commit | awk '{print $2}')
+
+if [ "$LATEST_SCRIPTS_COMMIT" != "$CURRENT_SCRIPTS_COMMIT" ]; then
+  echo -e 
+  read -p "You aren't running the current versions of the scripts. Are you sure you want to continue ? (Yy/Nn)" yn
+  echo -e
+
+  case $yn in
+        [Yy]* ) ;;
+        [Nn]* ) echo "Ok.Exiting."; exit;;
+        * ) echo "Please answer Yy / Nn ..."; exit;;
+    esac
+fi
+
+#See if the user has passed any command line arguments and if not show the menu
+if [ $# -eq 0 ]
+  then
+
+  show_menu #Show all the menu options
+
+  COLUMNS=12
+  PS3="Please select an action:"
+  options=("install" "observing_squad" "upgrade" "upgrade_squad" "upgrade_proxy" "remove_db" "start" "stop" "cleanup" "github_pull" "add_nodes" "get_logs" "quit")
+
+  select opt in "${options[@]}"
+  do
+
+  case $opt in
+
+  'install')
+    install
+    echo -e
+    read -n 1 -s -r -p "  Process finished. Press any key to continue..."
+    clear
+    show_menu
+    ;;
+
+  'observing_squad')
+    observers
+    echo -e
+    read -n 1 -s -r -p "  Process finished. Press any key to continue..."
+    clear
+    show_menu
+    ;;
+
+  'upgrade')
+    upgrade
+    echo -e
+    read -n 1 -s -r -p "  Process finished. Press any key to continue..."
+    clear
+    show_menu
+    ;;
+
+  'upgrade_squad')
+    upgrade_squad
+    echo -e
+    read -n 1 -s -r -p "  Process finished. Press any key to continue..."
+    clear
+    show_menu
+    ;;
+
+  'upgrade_proxy')
+    upgrade_proxy
+    echo -e
+    read -n 1 -s -r -p "  Process finished. Press any key to continue..."
+    clear
+    show_menu
+    ;;
+
+  'remove_db')
+    remove_db
+    echo -e
+    read -n 1 -s -r -p "  Process finished. Press any key to continue..."
+    clear
+    show_menu
+    ;;
+
+  'start')
+    start
+    echo -e
+    read -n 1 -s -r -p "  Process finished. Press any key to continue..."
+    clear
+    show_menu
+    ;;
+
+  'stop')
+    stop
+    echo -e
+    read -n 1 -s -r -p "  Process finished. Press any key to continue..."
+    clear
+    show_menu
+    ;;
+
+  'cleanup')
+    cleanup
+    echo -e
+    read -n 1 -s -r -p "  Process finished. Press any key to continue..."
+    clear
+    show_menu
+    ;;
+
+  'github_pull')
+    github_pull
+    echo -e
+    read -n 1 -s -r -p "  Process finished. Press any key to continue..."
+    clear
+    show_menu
+    ;;
+
+  'add_nodes')
+    add_node
+    echo -e
+    read -n 1 -s -r -p "  Process finished. Press any key to continue..."
+    clear
+    show_menu
+    ;;
+
+  'get_logs')
+    get_logs
+    echo -e
+    read -n 1 -s -r -p "  Process finished. Press any key to continue..."
+    clear
+    show_menu
+    ;;
+
+  'quit')
+    echo -e
+    echo -e "${GREEN}---> Exiting scripts menu...${NC}"
+    echo -e
+    break
+    ;;
+
+  esac
+
+  done
+
+else
+
+case "$1" in
+'install')
+  install
+  ;;
+
+'observing_squad')
+  observers
+  ;;
+
+'upgrade')
+  upgrade
+  ;;
+
+'upgrade_squad')
+  upgrade_squad
+  ;;
+
+'upgrade_proxy')
+  upgrade_proxy
+  ;;
+
+'remove_db')
+  remove_db
+  ;;
+
+'start')
+  start
+  ;;
+
+'stop')
+  stop
+  ;;
+
+'cleanup')
+  cleanup
+  ;;
+
+'github_pull')
+  github_pull
+  ;;
+
+'add_nodes')
+  add_node
+  ;;
+
+'get_logs')
+  get_logs
+  ;;
+  
+esac
+
+fi
